46322a0630f81bdb5e633d2b5a495b8b6514a6c3
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2019-03-12 21:27:39 +0000

    Fix by adding script type

--- src/server.js
+++ src/server.js
@@ -3,15 +3,14 @@ import express from 'express';
 import path from 'path';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
+import { StaticRouter, matchPath } from 'react-router-dom';
 import { ServerStyleSheet } from 'styled-components';
-import { Provider } from "react-redux";
-import createStore, { initializeSession } from "./store";
-// import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-
+import createStore, { initializeSession } from './store';
 import App from './components/App';
 import { logIn, loginCallback } from './LogIn';
+import routes from './routes';
 
 const app = express();
 
@@ -19,23 +18,26 @@ app.use(express.static(path.resolve(__dirname, '../dist')));
 app.use(cookieParser());
 
 app.get('/*', (req, res) => {
-  let preLoadedState = { loggedIn: false, spotifyAccessToken: null }
-  if (req.url === '/login') {
-    logIn(req, res);
-  } else if (req.url.split('?')[0] === '/callback') {
-    return loginCallback(req, res);
-  } else if (req.url.split('?')[0] === '/LogInSuccess') {
-    const token = req.cookies.spotifyAccessToken;
-    preLoadedState = { loggedIn: true,  spotifyAccessToken: token }
-  }
+  // let preLoadedState = { loggedIn: false, spotifyAccessToken: null }
+  // if (req.url === '/login') {
+  //   logIn(req, res);
+  // } else if (req.url.split('?')[0] === '/callback') {
+  //   return loginCallback(req, res);
+  // } else if (req.url.split('?')[0] === '/LogInSuccess') {
+  //   const token = req.cookies.spotifyAccessToken;
+  //   // preLoadedState = { loggedIn: true,  spotifyAccessToken: token }
+  // }
 
   const sheet = new ServerStyleSheet();
-  const context = {};
 
-  const store = createStore(preLoadedState);
+  const context = {};
+  const store = createStore();
+  // const store = createStore(preLoadedState);
+
+  store.dispatch(initializeSession());
 
   const jsx = (
-    <Provider store={ store }>
+    <Provider store={store}>
       <StaticRouter context={context} location={req.url}>
         <App />
       </StaticRouter>
@@ -43,8 +45,9 @@ app.get('/*', (req, res) => {
   );
   const reactDom = renderToString(sheet.collectStyles(jsx));
   const reduxState = store.getState();
-
   const styles = sheet.getStyleTags();
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(htmlTemplate(reactDom, styles, reduxState));
 });
 
@@ -62,12 +65,12 @@ function htmlTemplate(reactDom, styles, reduxState) {
             ${styles}
         </head>
 
-        <body style="margin:0">
+        <body>
             <div id="app">${reactDom}</div>
             <script>
-              window.REDUX_DATA = ${ JSON.stringify( reduxState ) }
+              window.REDUX_DATA = ${JSON.stringify(reduxState)}
             </script>
-            <script src="./app.bundle.js"></script>
+            <script type="text/jsx" src="./app.bundle.js"></script>
         </body>
         </html>
     `;
