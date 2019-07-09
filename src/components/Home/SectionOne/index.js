f18ffa5bdadead784088b743ea39ae2bbf39f6cd
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2019-07-09 22:23:18 +0100

    Add animation to homepage

--- src/components/Home/SectionOne/index.js
+++ src/components/Home/SectionOne/index.js
@@ -12,6 +12,7 @@ const Title = styled.div`
   display: flex;
   justify-content: center;
   ${media.phone`text-align: center;`}
+  position: relative;
 `;
 
 const MainHeading = styled.h2`
@@ -29,6 +30,7 @@ const SubHeading = styled.p`
   font-size: 24px;
   letter-spacing: 10px;
   text-shadow: 1px 1px 1px #000000;
+  padding-left: 7px;
 
   ${media.desktop`font-size: 16px;`}
   ${media.phone`font-size: 18px;`}
@@ -42,16 +44,57 @@ const Container = styled.div`
   justify-content: space-around;
 `;
 
+const Icon = styled.img`
+  height: 64px;
+  transform: translateY(8px);
+`;
+
+const flickerAnimation = keyframes`
+  0% {
+    transform: translate(-10px, 10px);
+    color: white;
+  }
+  50% {
+    color: grey;
+
+  }
+  100% {
+    color: rgba(0, 0, 0, 0);
+  }
+`;
+
+const MusicNote = styled.div`
+  position: absolute;
+  top: 32%;
+  left: 46%;
+  font-size: 24px;
+  animation: ${flickerAnimation} 3s infinite ease-out;
+  color: rgba(0, 0, 0, 0);
+`;
+
 const SectionOne = () => {
   return (
     <Container>
       <Title>
-        <MainHeading>People's Party Playlist</MainHeading>
+        <MainHeading>
+          People's P<Icon src={'images/jukebox.svg'} />
+          rty Playlist
+        </MainHeading>
         <SubHeading>The Ultimate online jukebox experience</SubHeading>
+        <MusicNote>&#9835;</MusicNote>
       </Title>
+
       <Mouse />
     </Container>
   );
 };
 
 export default SectionOne;
+
+// jukebox icon credit
+{
+  /* <div>Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" title="Vitaly Gorbachev">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */
+}
+{
+  /* <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */
+}
