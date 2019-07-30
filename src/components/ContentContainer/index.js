92c3d0f71fd048f12ce2a1d6496bfad16f12a785
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2019-07-30 10:48:31 +0100

    Center content on mobile

--- src/components/ContentContainer/index.js
+++ src/components/ContentContainer/index.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 import { media } from '../../globalStyles';
 
 const Container = styled.div`
   width: 70%;
-  ${media.desktop`width: 95%;`}
+  ${media.desktop`
+    width: 95%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  `}
 `;
 
-const ContentContainer = ({children}) => {
-  return (
-    <Container>
-      {children}
-    </Container>
-  )
-}
+const ContentContainer = ({ children }) => {
+  return <Container>{children}</Container>;
+};
 
-export default ContentContainer
+export default ContentContainer;
