3547ed980c8507b5b29096a0e843e51aff193034
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2019-08-30 13:22:17 +0100

    Remove old file

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,6 @@ export * from './auth';
 export * from './playback';
 export * from './playlist';
 export * from './playlistFetch';
-export * from './playlistsFetch';
 export * from './searchFetch';
 export * from './callAPIMiddleware';
 export * from './user';
